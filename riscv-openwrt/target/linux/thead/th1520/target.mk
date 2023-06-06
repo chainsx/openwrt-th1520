37b1114ca099be17b3e4c73fa65e3ea611728c3c
chainsx <chainsx@outlook.com>
2023-06-06 15:21:44 +0800

    fix

--- riscv-openwrt/target/linux/thead/th1520/target.mk
+++ riscv-openwrt/target/linux/thead/th1520/target.mk
@@ -1,7 +1,7 @@
 ARCH:=riscv64
 SUBTARGET:=th1520
 BOARDNAME:=Sipeed LicheePi 4A riscv64
-CPU_TYPE:=rv64
+CPU_TYPE:=riscv64
 
 define Target/Description
 	Build firmware images for Thead riscv64 boards.
